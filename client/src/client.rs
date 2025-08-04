dcab1cacc26ff6c5f7493bedbea53bb8e75beca7
Mykola Titov <kolatitov132@gmail.com>
2025-08-05 02:43:31 +0300

    Looped handle_client(). Added client realization

--- client/src/client.rs
+++ client/src/client.rs
@@ -1,3 +1,25 @@
+use std::io::{self, Write, Read};
+use std::net::{TcpStream};
+
 fn main() {
-    println!("Hello Client!");
+    let server_addr = "192.168.31.10:8080";
+    
+    let mut stream = TcpStream::connect(server_addr).expect("Cannot connect to the server!");
+
+    println!("Connected to the {}", server_addr);
+    
+    loop {
+        print!("Enter message: ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Cannot read the message!");
+    
+        stream.write_all(input.as_bytes()).expect("Cannot send message!");
+    
+        let mut buffer = [0; 1024];
+        let n = stream.read(&mut buffer).expect("Cannot read unswer from server!");
+    
+        let response = String::from_utf8_lossy(&buffer[..n]);
+        println!("Unswer from server: {}", response);
+    }
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -3,11 +3,13 @@ use std::net::{TcpListener, TcpStream};
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).expect("Failed to read from client!");
-    let request = String::from_utf8_lossy(&buffer[..]);
-    println!("Received request: {}", request);
-    let response = "Hello, Client!".as_bytes();
-    stream.write(response).expect("Failed to write response!");
+    loop {
+        stream.read(&mut buffer).expect("Failed to read from client!");
+        let request = String::from_utf8_lossy(&buffer[..]);
+        println!("Received request: {}", request);
+        let response = "Hello, Client!".as_bytes();
+        stream.write(response).expect("Failed to write response!");
+    }
 }
 
 fn main() {
